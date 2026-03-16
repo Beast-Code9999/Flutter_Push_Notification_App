b3e6fc1e06d9adf7e2bec353875d9dcfaea346a7
beastcode9999 <beastcode9999@gmail.com>
2026-03-16 16:25:13 +0930

    feat: add sign up form with AuthForm with Firebase user creation and Firestore

--- push_notification_app/lib/screens/signup_screen.dart
+++ push_notification_app/lib/screens/signup_screen.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:push_notification_app/screens/login_screen.dart';
+import 'package:push_notification_app/services/user_services.dart';
+import 'package:push_notification_app/widgets/auth_form.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -10,6 +14,48 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AuthForm(
+      title: "sing up",
+      onSubmit: (email, password) async {
+        try { 
+          // Attempt to create a new user with email and password
+          final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+            email: email,
+            password: password,
+          );
+          // save user to firestore
+          await UserServices.createUser(
+            userCredential.user!.uid,
+            email,
+            'student',
+          );
+
+          Navigator.pop(context); // go back to auth gate after successful sign up, which handles the screen navigation
+
+        } 
+        
+        on FirebaseAuthException catch (e) { 
+          // handle any errors returned by Firebase
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(e.message ?? "Login failed")),
+          );
+        } 
+        catch (e) {
+          // handle any errors like network or runtime errors
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Network error. Please try again.")),
+          );
+        }
+      },
+      bottomLink: TextButton(
+        onPressed: () {
+          Navigator.push(
+            context, 
+            MaterialPageRoute(builder: (context) => LoginScreen())
+          );
+        }, 
+        child: Text("Sign in if you already have an account.")
+      ),
+    );
   }
 }
