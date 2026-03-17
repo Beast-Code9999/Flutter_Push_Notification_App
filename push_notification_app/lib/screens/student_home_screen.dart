ff5b717e7c9e66a189c44a23ea4b653209d610c2
beastcode9999 <beastcode9999@gmail.com>
2026-03-17 15:47:10 +0930

    feat: create a list displaying all existing notifications

--- push_notification_app/lib/screens/student_home_screen.dart
+++ push_notification_app/lib/screens/student_home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:push_notification_app/models/app_notification.dart';
 
 class StudentHomeScreen extends StatefulWidget {
   const StudentHomeScreen({super.key});
@@ -14,7 +16,7 @@ class _StudentHomeScreenState extends State<StudentHomeScreen> {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(
+          IconButton( // logout button
             onPressed: () {
               FirebaseAuth.instance.signOut();
             }, 
@@ -22,7 +24,38 @@ class _StudentHomeScreenState extends State<StudentHomeScreen> {
           )
         ],
       ),
-      body: Text("Hello world student"),
+      body: StreamBuilder( // listen to notification collection and display each notification in a listview
+        stream: FirebaseFirestore.instance.collection("notifications").snapshots(), 
+        builder: (context, snapshot) {
+          // loading state
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator(),);
+          }
+          // Error state
+          if (snapshot.hasError) {
+            return Center(child: Text("Something went wrong!"),);
+          }
+          // handgle no data/notification
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(child: Text("No notification"),);
+          }
+          // Data/notification exists
+          final docs = snapshot.data!.docs; // list of firestore documents, each doc is a snapshot from firebase firestore
+
+          return ListView.builder( // display list of notifcations
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              final data = docs[index].data(); // each document on the specific index in extracted format
+              final notification = AppNotification.fromMap(data); // convert extracted format to dart object
+
+              return ListTile(
+                title: Text(notification.title),
+                subtitle: Text(notification.content),
+              );
+            }
+          );
+        }
+      ),
     );
   }
 }
