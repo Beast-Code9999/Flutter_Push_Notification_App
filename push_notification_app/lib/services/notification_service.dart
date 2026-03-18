6ba116c0563f70855f1044645cf8261bc29ecd84
beastcode9999 <beastcode9999@gmail.com>
2026-03-18 11:09:51 +0930

    feat: add comments explaining why notifcation_service.dart is empty

---
+++ push_notification_app/lib/services/notification_service.dart
@@ -0,0 +1,7 @@
+// this file was suppose to hold the Firebase Cloud Messaging (FCM) logic
+// It handles things such as: 
+//  Getting and storing FCM token
+//  Listening for incoming messages when the app is in the foreground
+//  Handling notification taps to navigate to the right screen
+// 
+// However, due to payment setup requirement, this process has been skipped
